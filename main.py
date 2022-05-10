ce3d692e81f6ad97489139e8999c5925837ad912
Bram <brvolbeda@gmail.com>
2022-05-10 17:31:31 +0200

    PEP8 added

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 # TODO implement the PINN3.0 Code
 import torch
+print('test complete')
+
+### naming conventions 
+# Function - my_function
+# Variable - my_variable
+# Class - MyClass
+# constant - MY_CONSTANT
+# module - my_module.py
+# package - mypackage
+
+# If line breaking needs to occur around binary operators, like + and *, it should occur before the operator
+# PEP 8 recommends that you always use 4 consecutive spaces to indicate indentation.
+# Line up the closing brace with the first non-whitespace character of the previous line 
+# Surround docstrings with three double quotes on either side, as in """This is a docstring"""
+# - Write them for all public modules, functions, classes, and methods.
+# - Put the """ that ends a multiline docstring on a line by itself
+# Use .startswith() and .endswith() instead of slicing
+
 
 
 
